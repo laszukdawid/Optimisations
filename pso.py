b0d6269660368e91384a725d5daebe68c5c3dc5d
Dawid Laszuk <laszukdawid@gmail.com>
2017-02-04 11:05:30 +0100

    - Halton sequence used to generate initial positions. - Added perturbations, which kick off stagnated positions. - Compatible with Python 3. - Improved documentation.

--- pso.py
+++ pso.py
@@ -3,11 +3,10 @@
 #
 # Author: Dawid Laszuk
 # Contact: laszukdawid@gmail.com
-#          d.laszuk@pgr.reading.ac.uk
 #
 # Feel free to contact for any information.
 #
-# Last update: 10/03/2015
+# Last update: 04/02/2017
 #
 # You can cite this code by referencing:
 #   D. Laszuk, "Python implementation of Particle 
@@ -31,30 +30,75 @@
 #  MA 02110-1301, USA.
 #  
 
+from __future__ import print_function
 
 from scipy.integrate import ode
 import numpy as np
 import pylab as py
 
+def halton_sequence(size, dim):
+        
+    seq = np.zeros( (dim,size) )
+    primeGen = next_prime()
+    next(primeGen)
+    
+    for d in range(dim):
+        base = next(primeGen)
+        seq[d] = np.array([vdc(i, base) for i in xrange(size)])
+        
+    return seq
+    
+def next_prime():
+    def is_prime(num):
+        "Checks if num is a prime value"
+        for i in range(2,int(num**0.5)+1):
+            if(num % i)==0: return False
+        return True
+    
+    prime = 3
+    while(1):
+        if is_prime(prime):
+            yield prime
+        prime += 2
+    
+def vdc(n, base=2):
+    vdc, denom = 0,1
+    while n:
+        denom *= base
+        n, remainder = divmod(n, base)
+        vdc += remainder / float(denom)
+    return vdc
+
+
 
 class Particle:
     def __init__(self, dim=10):
-        pass
         self.__dim = dim
 
 class PSO:
-    def __init__(self, func, bounds, initPos=None, nPart=10):
+    def __init__(self, func, bounds, initPos=None, nPart=None):
+        """
+        Performs Particle Swarm Optimisation to find the minimum of 
+        `func` function within provided parameters `bounds` range.
         
-        # number of particles in swarm
-        self.nPart = 100
+        The `func` should be a callable, and `bounds` a list-like 
+        where the first and second indices contain min and max bounds,
+        respectively.
+        
+        Additionally one can set up `initPos` initial positions (see
+        self.setInitPos) and the number `nPart` of used particles.
+        """
         
         # Params
         self.epsError = 1.
-        self.maxGen = 500
+        self.maxGen = 6000
+        self.minRepetition = 100
+        self.maxRepetition = 100
         
-        self.w = 0.2
-        self.phiP = 0.2
-        self.phiG = 0.1
+        self.w = 0.01
+        self.phiP = 0.2  # Local best
+        self.phiG = 0.1 # Global best
+        self.phiN = 0.01 # Noise affect
                         
         # Function to be minimised
         self.problem = func
@@ -64,29 +108,61 @@ class PSO:
         self.maxBound = np.array(bounds[1])
 
         self.dim = len(bounds[0])
+
+        # Noise preparation
+        self.nMean = np.zeros(self.dim)
+        cov = np.sqrt(self.maxBound - self.minBound)/2
+        self.nCov = np.diag(cov)
+        MN = np.random.multivariate_normal
+        cov[cov==0] = 1
+        norm = np.sum(cov)*2*np.pi
+        self.noise = lambda: (MN(self.nMean, self.nCov, 1)/norm).flatten()
+        
+        # Speed vector
+        self.speed = np.ones(self.dim)
+        # Number of particles in swarm
+        if nPart==None:
+            nPart = 100
+        self.nPart = nPart
         
         # Initial positions
         if initPos!=None:
-            self.initPos = np.array(initPos).reshape((-1,self.dim))        
-        else:
-            self.initPos = initPos
+            initPos = np.array(initPos).reshape((-1,self.dim))
+        self.initPos = initPos
 
+    def setInitPos(self, initPos):
+        """
+        Variable `initPos` sets initial values for the PSO.
+        It is a 2D list of (iParts, params) shape.
+        
+        If `iParts` is smaller than self.nPart then it will initiate  
+        first iParts with provided parameters and the rest particles
+        will be assigned with random params within boundaries.
+        """
+        initPos = np.array(initPos).reshape((-1,self.dim))        
+        self.initPos = initPos
 
     def __initPart(self):
-        """Initiate particles.
-        """
+        """Initiate particles."""
+        
+        _size = initPos.shape[0] if self.initPos!=None else 0
+        self.seq = halton_sequence(self.nPart-_size+3, self.dim)
          
         # Create particles
-        self.Particles = []
-        for i in range(self.nPart):
-            self.Particles.append( Particle(self.dim) )
+        self.Particles = [Particle(self.dim) for i in range(self.nPart)]
         
+        # Position in generated semi-random sequence
+        seqPos = 0
+
         # Initiate pos and fit for particles
         for part in self.Particles:
             
             # Initial position
             if self.initPos == None:
-                part.pos = np.random.random(self.dim)*self.maxBound - self.minBound
+
+                part.pos = self.seq[:,seqPos]*(self.maxBound-self.minBound)
+                part.pos += self.minBound
+                seqPos += 1
             else:
                 part.pos = self.initPos[0,:]
                 self.initPos = np.delete(self.initPos, 0,0)
@@ -95,7 +171,7 @@ class PSO:
                 if len(self.initPos) == 0: self.initPos = None
             
             # Initial velocity
-            part.vel = np.random.random(self.dim)*(self.maxBound - self.minBound)
+            part.vel = np.random.random(self.dim)*(np.sqrt(self.maxBound - self.minBound)/2)
             part.vel *= [-1., 1.][np.random.random()>0.5]
             
             # Initial fitness
@@ -112,17 +188,22 @@ class PSO:
                 self.globBestPos = part.pos
     
     def update(self):
-        
+        """Updates each step."""
         for part in self.Particles:
             
             # Gen param
             rP, rG = np.random.random(2)
+            # Replacing random values with speed vector
             
             w, phiP, phiG = self.w, self.phiP, self.phiG
             
             # Update velocity
             v, pos = part.vel, part.pos
-            part.vel = self.w*v + phiP*rP*(part.bestPos-pos) + phiG*rG*(self.globBestPos-pos)
+            part.vel = self.w*v
+            part.vel += phiP*rP*(part.bestPos-pos) # local best update
+            part.vel += phiG*rG*(self.globBestPos-pos) # global best update
+            
+            part.vel += self.phiN*self.noise() # perturbation
             
             # New position
             part.pos += part.vel
@@ -150,10 +231,7 @@ class PSO:
                 self.globBestFit = part.fitness
                 self.globBestPos = part.pos
             
-    def finished(self):
-        """What is returned when searched is finished.
-        """
-        
+    def getGlobalBest(self):
         return self.globBestPos, self.globBestFit
     
     def optimize(self):
@@ -163,23 +241,37 @@ class PSO:
         
         # Initiate particles
         self.__initPart()
+        self.lastGlobBestFit = 0
+        self.changeCounter = 0
         
         idx = 0
         while(idx < self.maxGen):
-            print "Gen: {}/{}  -- best = {}".format(idx, self.maxGen, self.globBestFit)
+            print("Gen: {}/{}  -- best = {}".format(idx, self.maxGen, self.globBestFit))
             
             # Perform search
             self.update()
             
             # Acceptably close to solution
             if self.globBestFit < self.epsError:
-                return self.finished()
+                return self.getGlobalBest()
+            
+            if self.globBestFit == self.lastGlobBestFit and \
+                idx > self.minRepetition:
+                
+                self.changeCounter += 1
+                if self.changeCounter == self.maxRepetition:
+                    print("Obtained limit of repeating the same value.")
+                    print("Stopping calculations.")
+                    break
+            else:
+                self.changeCounter = 0
+            self.lastGlobBestFit = self.globBestFit
             
             # next gen
             idx += 1
         
         # Search finished
-        return self.finished()
+        return self.getGlobalBest()
         
 
 #################################
@@ -192,7 +284,7 @@ if __name__ == "__main__":
     S+= (np.random.random(N)-0.5)*0.05
 
     rec = lambda a: a[0]*t*np.sin(a[1]*np.pi*t**2 +a[2])*np.exp(-a[3]*t**2)
-    minProb = lambda a: np.sum(np.abs(S-rec(a)))
+    minProb = lambda a: np.sum(np.abs(S-rec(a))**2)
 
     numParam = 4
     bounds = ([0]*numParam, [10]*numParam)
@@ -200,8 +292,8 @@ if __name__ == "__main__":
     pso = PSO(minProb, bounds)
     bestPos, bestFit = pso.optimize()
 
-    print 'bestFit: ', bestFit
-    print 'bestPos: ', bestPos
+    print('bestFit: ', bestFit)
+    print('bestPos: ', bestPos)
 
     ############################
     # Visual results representation
